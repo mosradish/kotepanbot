00b71d5ff639f2c3f71384eea0bd8bb88e4b1b5c
mosradish <mosradish@gmail.com>
2025-08-10 15:00:57 +0900

    fix display

--- discord.py
+++ discord.py
@@ -1,29 +1,43 @@
-# discord.py
-import os
-import json
-import requests
-from dotenv import load_dotenv
-
-load_dotenv()
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
-
 def send_discord_embed(tweet, target_username):
+    author = tweet.get('author_username', target_username)
+    tweet_text = tweet.get("text", "")
+    if len(tweet_text) > 400:
+        tweet_text = tweet_text[:397] + "..."
+
     embed = {
         "username": "Twitter通知Bot",
         "avatar_url": "https://abs.twimg.com/icons/apple-touch-icon-192x192.png",
         "embeds": [
             {
-                "title": f"📢 @{tweet.get('author_username', target_username)} さんの新しいツイート",
-                "url": f"https://twitter.com/{tweet.get('author_username', target_username)}/status/{tweet['id']}",
-                "description": tweet.get("text", "")[:2048],
+                "title": f"📢 @{author} さんの新しいツイート",
+                "url": f"https://twitter.com/{author}/status/{tweet['id']}",
+                "description": tweet_text,
                 "color": 0x1DA1F2,
                 "footer": {
                     "text": "Twitter → Discord 自動通知"
                 },
-                "timestamp": tweet.get("created_at")
+                "timestamp": tweet.get("created_at"),
+                "fields": [
+                    {
+                        "name": "投稿日時",
+                        "value": tweet.get("created_at"),
+                        "inline": True
+                    },
+                    {
+                        "name": "ツイートID",
+                        "value": tweet["id"],
+                        "inline": True
+                    }
+                ],
             }
         ]
     }
+
+    # 画像URLがある場合 embedに追加
+    media_url = tweet.get("media_url")  # 例：tweetに直接 media_url を入れてください
+    if media_url:
+        embed["embeds"][0]["image"] = {"url": media_url}
+
     headers = {"Content-Type": "application/json"}
     resp = requests.post(DISCORD_WEBHOOK_URL, data=json.dumps(embed), headers=headers)
     resp.raise_for_status()
